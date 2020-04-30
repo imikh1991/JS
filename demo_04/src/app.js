4b457affd03004934d85a3ababe375b8940ec86c
Ilya Mikhaylykov <ilya.mikhaylyukov@gmail.com>
2020-04-30 18:35:30 +0800

    Мои файлы JavaScript

---
+++ demo_04/src/app.js
@@ -0,0 +1,13 @@
+const start = 0;
+const stop = 100;
+const step = 1;
+
+let x = start;
+while (x < stop) {
+    console.log(' НАШ ИКС =');
+    console.log(x);
+    x+=step;
+}
+console.log('ТЕПЕРЬ НАШ ИКС = ');
+console.log(x);
+
